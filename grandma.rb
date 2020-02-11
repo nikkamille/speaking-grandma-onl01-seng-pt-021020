1e04a11ddf7f52fbca7d971dd31aba171527e696
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-11 03:48:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,4 +14,10 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  else 
+    "NO, NOT SINCE 1938!"
+  end
 end
